326d1a69743dd4a23286cacb793478ac7fcbac74
DELL-5557\Allan Horst <allanhorstwillig@gmail.com>
2016-10-25 20:21:46 -0200

    Adicionado um monte de coisa que não quero ficar escrevendo

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,59 @@
 angular.module('worstFitApp', [])
 
-.controller('appController',  function($scope){
-	$scope.test = 'Not implemented yet :( '
+.controller('appController',  function($scope) {
+	$scope.click = function(item) {
+		alert(item.name)
+	}
+
+	$scope.programs = [
+		{
+			size: 50,
+			icon: "footage/chrome-icon.png",
+			name: "Google Chrome",
+			id: 1
+		}, 
+		{
+			size: 30,
+			icon: "footage/ie-icon.png", 
+			name: "Internet Explorer", 
+			id: 2
+		}, 
+		{
+			size: 10,
+			icon: "footage/excel-icon.png",
+			name: "Excel", 
+			id: 3
+		},
+		{
+			size: 5, 
+			icon: "footage/word-icon.png",
+			name: "Word", 
+			id: 4
+		},
+		{
+			size: 7, 
+			icon: "footage/msn-icon.png",
+			name: "MSN", 
+			id: 5
+		},
+		{
+			size: 3, 
+			icon: "footage/spotify-icon.png",
+			name: "Spotify", 
+			id: 6
+		},
+		{
+			size: 5, 
+			icon: "footage/premiere-icon.png",
+			name: "Adobe Premiere", 
+			id: 7
+		},
+		{
+			size: 5, 
+			icon: "footage/ps-icon.png",
+			name: "Adobe Photoshop", 
+			id: 8
+		}
+	]
+
 })
