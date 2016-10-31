dabb5b7bcc5899039edf71b977e18a464554bf68
DELL-5557\Allan Horst <allanhorstwillig@gmail.com>
2016-10-31 07:30:28 -0200

    Allahu akbar

--- js/controller.js
+++ js/controller.js
@@ -2,18 +2,71 @@ angular.module('worstFitApp', [])
 
 .controller('appController',  function($scope) {
 	$scope.click = function(item) {
-		// alert(item.name)
+		alert(item.name)
 	}
 
+	$scope.memory1 = {
+		list: [],
+		freeSpace: 100,
+		full: false
+	}
+
+	$scope.memory2 = {
+		list: [],
+		freeSpace: 100,
+		full: false
+	}
+
+	initialize($scope.memory1);
+	initialize($scope.memory2);
+
+	console.log($scope.memory1);
+	console.log($scope.memory2);
+
+	function initialize(memory) {
+		var max = 20;
+		var min = 4;
+
+		var i = 1;
+		
+		while(!memory.full) {
+			let type = parseInt(getRandomNumber(0, 2))	;
+
+			let obj = {
+				size: parseInt(getRandomNumber(min, max)),
+				type: type == 1 ? 'USED' : 'FREE',
+				description: type == 1 ? 'Process ' + i : 'Free space',
+				id: Number(parseInt(getRandomNumber(1000, 1000000))).toString(16)
+			}
+
+			if (obj.size <= memory.freeSpace) {
+				memory.list.push(obj);
+			}
+			
+			memory.freeSpace -= obj.size;
+
+			if (memory.freeSpace == 0) {
+				memory.full = true;
+			} else if (memory.freeSpace < 20) {
+				max = memory.freeSpace;
+				min = memory.freeSpace;
+			}
+			i++;
+		}
+	}
+
+	function getRandomNumber(min, max) {
+		return Math.random() * (max - min) + min;
+	}
 	$scope.programs = [
 		{
-			size: 50,
+			size: 20,
 			icon: "footage/chrome-icon.png",
 			name: "Google Chrome",
 			id: 1
 		}, 
 		{
-			size: 30,
+			size: 16,
 			icon: "footage/ie-icon.png", 
 			name: "Internet Explorer", 
 			id: 2
@@ -25,7 +78,7 @@ angular.module('worstFitApp', [])
 			id: 3
 		},
 		{
-			size: 5, 
+			size: 9, 
 			icon: "footage/word-icon.png",
 			name: "Word", 
 			id: 4
@@ -37,7 +90,7 @@ angular.module('worstFitApp', [])
 			id: 5
 		},
 		{
-			size: 3, 
+			size: 11, 
 			icon: "footage/spotify-icon.png",
 			name: "Spotify", 
 			id: 6
@@ -49,7 +102,7 @@ angular.module('worstFitApp', [])
 			id: 7
 		},
 		{
-			size: 5, 
+			size: 18, 
 			icon: "footage/ps-icon.png",
 			name: "Adobe Photoshop", 
 			id: 8
